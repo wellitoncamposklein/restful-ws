785312ee94e5b24b91d0a6ec54e59a2dd690fef6
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-21 18:45:02 -0200

    Aula 10

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,26 +1,26 @@
 const db = require('../services/mysql')
 
 const routes = (server) => {
-    server.get('categoria', (req, res, next) => {
-        db.categories().all()
-            .then(categories => {
-                res.send(categories)
-                next()
-            })
-            .catch(error => {
-                res.send(error)
-                next()
-            })
+    server.get('categoria', async (req, res, next) => {
+        try {
+            res.send(await db.categories().all())
+        }catch (error) {
+            res.send(error)
+            next()
+        }
     })
+
     server.post('categoria', (req, res, next) => {
         const { name } = req.params
         res.send(name)
         next()
     })
+
     // server.put('categoria', (req, res, next) => {
     //     res.send()
     //     next()
     // })
+
     // server.delete('categoria', (req, res, next) => {
     //     res.send()
     //     next()
