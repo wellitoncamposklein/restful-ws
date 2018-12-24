bf8c83b3e0fa9e65e8261da4506372c1e9a05dba
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-24 16:26:48 -0200

    Aula 17

--- src/services/mysql/tests/categories.test.js
+++ src/services/mysql/tests/categories.test.js
@@ -1,24 +1,29 @@
-require('dotenv').config()
-
 const test = require('ava')
-
-const mysqlServer = require('mysql')
-
-const connection = mysqlServer.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_TEST_DATABASE
-})
-
-const errorHandler = (error, msg, rejectFunction) => {
-    console.error(error)
-    rejectFunction({ error: msg })
-}
+const { connection, errorHandler } = require('./setup')
 
 const categories = require('../categories')({ connection, errorHandler })
 
+const create = () => categories.save('category-save')
+const update = () => categories.update(1,'category-updated')
+const del = () => categories.del(1)
+
+test.beforeEach(() => connection.query('TRUNCATE TABLE categories'))
+test.after.always(() => connection.query('TRUNCATE TABLE categories'))
+
 test('Creation category', async t => {
-    const result = await categories.save('category-save')
+    const result = await create()
     t.is(result.category.name, 'category-save')
+})
+
+test('Update category', async t => {
+    await create()
+    const updated = await update()
+    t.is(updated.category.name, 'category-updated')
+    t.is(updated.affectedRows, 1)
+})
+
+test('Remove category', async t => {
+    await create()
+    const removed = await del()
+    t.is(removed.affectedRows, 1)
 })
