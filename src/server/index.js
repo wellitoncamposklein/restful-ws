b290cd01000b874840b4484f6514bb18243381d8
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-26 16:33:51 -0200

    Aula 22

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,14 @@ const cors = require('./cors')
 server.pre(cors.preflight)
 server.use(cors.actual)
 server.use(restify.plugins.bodyParser())
+server.use((req, res, next) => {
+    if (!req.headers['x-access-token']) {
+        res.send(403,{ error: 'Token nao firnecido' })
+        return false
+    }
+    //alguma verificacao no token
+    next()
+})
 
 routes(server)
 
